62a5e10f96d3c4d586acca350d8ba7f99e8c308e
EC2 Default User <ec2-user@ip-10-247-1-84.ap-southeast-1.compute.internal>
2020-11-16 10:15:28 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3f9b613886d6877e6018429a20c1d86'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '16c6e1ef-dc6c-4675-82e4-df3597fe9c96'
+};
